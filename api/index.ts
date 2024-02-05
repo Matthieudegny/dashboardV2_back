14f00f3d80eb273d929ca934bc83a29ccff403d4
Matthieudegny <matthieu.dev.degny@gmail.com>
2024-02-05 19:22:14 +0100

    first commit

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+// api/index.ts
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const server = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  await app.init();
+}
+
+bootstrap();
+
+export default server;
